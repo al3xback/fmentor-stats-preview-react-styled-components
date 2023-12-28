7c25262a24dd99c4edf661f2ecc6863bcc285447
alex <al3xback@gmail.com>
2023-12-28 10:26:38 +0700

    fixed and sorted properties

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,8 +7,8 @@ const CardShell = styled.article`
 	flex-direction: column;
 	width: 100%;
 	max-width: 28em;
-	margin: 2.6666666667em 0 4em;
 	background-color: hsl(244, 38%, 16%);
+	margin: 2.6666666667em 0 4em;
 	border-radius: 8px;
 	overflow: hidden;
 
